d1fa34666983ff87a94656ac8910908375827d62
doongidoong <wlgns3398@naver.com>
2022-04-16 20:44:28 +0900

    Circular queue

--- ex021.c
+++ ex021.c
@@ -12,7 +12,7 @@ typedef struct {
 element queue[MAX_QUEUE_SIZE];
 int rear = 0;
 int front = 0;
-
+int size= 0;
 void addq(element);
 element deleteq();
 void queueFull();
@@ -23,42 +23,44 @@ void main() {
   int i;
   element e;
 
-  for(i = 0; i < 8; i++) {
+  for(i = 0; i < 10; i++) {
     e.key = i;
     addq(e);
-    printf("key %2d inserted into the queue. (front: %2d rear: %2d)\n", e.key, front, rear);
+    printf("key %2d inserted into the queue. (front: %2d rear: %2d size: %2d)\n", e.key, front, rear,size);
   }
 
-  for(i = 0; i < 8; i++) {
+  for(i = 0; i < 11; i++) {
     e = deleteq();
-    printf("key %2d  deleted from the queue. (front: %2d rear: %2d)\n", e.key, front, rear);
+    printf("key %2d  deleted from the queue. (front: %2d rear: %2d size: %2d)\n", e.key, front, rear,size);
   }
 
   for(i = 8; i < 16; i++) {
     e.key = i;
     addq(e);
-    printf("key %2d inserted into the queue. (front: %2d rear: %2d)\n", e.key, front, rear);
+    printf("key %2d inserted into the queue. (front: %2d rear: %2d size: %2d)\n", e.key, front, rear,size);
   }
 
   for(i = 8; i < 16; i++) {
     e = deleteq();
-    printf("key %2d  deleted from the queue. (front: %2d rear: %2d)\n", e.key, front, rear);
+    printf("key %2d  deleted from the queue. (front: %2d rear: %2d size: %2d)\n", e.key, front, rear,size);
   }
 }
 
 void addq(element item) {
   /* add an item to the queue */
   rear = (rear + 1) % MAX_QUEUE_SIZE;
-  if(front == rear) 
-    queueFull();
+  if(size==MAX_QUEUE_SIZE) queueFull();
+  // if(front == rear)queueFull();
+  size++;
   queue[rear] = item;
 }
 
 element deleteq() {
   /* remove element at the front of the queue */
-  if(front == rear)
+  if(size==0)
     return queueEmpty();
   front = (front + 1) % MAX_QUEUE_SIZE;
+  size--;
   return queue[front];
 }
 
