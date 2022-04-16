78f5abfe0e1ae2cef6a19df16c810194c5296b87
doongidoong <wlgns3398@naver.com>
2022-04-16 23:44:33 +0900

    연결리스트 sparse

--- ex033.c
+++ ex033.c
@@ -69,32 +69,32 @@ matrix_pointer mread() {
     for(i=0; i<num_terms; i++) {
       printf("Enter row, column and value:\n");
       scanf("%d %d %d", &row, &col, &value);
-      if(row > current_row) {
-        last->right = hdnode[current_row];
-        current_row = row;
-        last = hdnode[row];
+      if(row > current_row) {//row가 바뀐다면
+        last->right = hdnode[current_row];//현재의 마지막이 헤드를 가르키게 함
+        current_row = row; // row를 바꿔줌
+        last = hdnode[row];//맨끝이 Hd을 갱신
       }
       temp = new_node();
       temp->tag = entry;
       temp->u.entry.row = row;
       temp->u.entry.col = col;
       temp->u.entry.value = value;
-      last->right = temp;
-      last = temp;
-      hdnode[col]->u.next->down = temp;
-      hdnode[col]->u.next = temp;
+      last->right = temp;//오른쪽으로 이어줌
+      last = temp;//오른쪽을 갱신
+      hdnode[col]->u.next->down = temp;//column 기준으로 맨 끝의 다운이 temp를 가르키도록, 아래쪽을 이어주는 과정 
+      hdnode[col]->u.next = temp;//column 기준 맨 끝을 갱신
     }
 
     // close last row
-    last->right = hdnode[current_row];
+    last->right = hdnode[current_row]; //제일 마지막 원소가 헤더를 가르키도록
     // close all column lists
     for(i=0; i<num_cols; i++)
-      hdnode[i]->u.next->down = hdnode[i];
+      hdnode[i]->u.next->down = hdnode[i]; // 마지막 row의 down을 헤드로 이어줌
     // link all head nodes together
     for(i=0; i<num_heads-1; i++)
-      hdnode[i]->u.next = hdnode[i+1];
-    hdnode[num_heads-1]->u.next = node;
-    node->right = hdnode[0];
+      hdnode[i]->u.next = hdnode[i+1]; // 헤드 노드의 다음을 다음 헤드 노드로 갱신
+    hdnode[num_heads-1]->u.next = node;//마지막 노드는 H를 가르키도록함
+    node->right = hdnode[0]; // H와 H0을 이어줌
   }
   return node;
 }
